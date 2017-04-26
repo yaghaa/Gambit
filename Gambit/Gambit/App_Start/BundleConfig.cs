260b3b1e7f390012dbfe63980963360d2881f874
trojanagent <trojanagent@interia.pl>
2017-04-26 23:20:36 +0200

    Wielkie 322 zmian :)
    Faza pierwsza przenoszenia projektu do MVC. Jeszcze nie działają jsy

--- Gambit/Gambit/App_Start/BundleConfig.cs
+++ Gambit/Gambit/App_Start/BundleConfig.cs
@@ -17,15 +17,28 @@ namespace Gambit
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/allscripts").Include(
+                         "~/Scripts/sb-admin-2",
+                         "~/Scripts/locale-all",
+                         "~/Scripts/gulpfile",
+                         "~/Scripts/gcal",
+                         "~/Scripts/fullcalendar",
+                         "~/Scripts/calendar",
+                         "~/Scripts/main"
+                         ));
+
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/fullcalendar.css",
+                      "~/Content/css/sb-admin-2.css",
+                      "~/Content/css/timeline.css"));
         }
     }
 }
---
+++ startbootstrap-sb-admin-2-gh-pages/startbootstrap-sb-admin-2-gh-pages/scripts/main.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
+
+    function htmlbodyHeightUpdate(){
+		var height3 = $( window ).height()
+		var height1 = $('.nav').height()+50
+		height2 = $('.main').height()
+		if(height2 > height3){
+			$('html').height(Math.max(height1,height3,height2)+10);
+			$('body').height(Math.max(height1,height3,height2)+10);
+		}
+		else
+		{
+			$('html').height(Math.max(height1,height3,height2));
+			$('body').height(Math.max(height1,height3,height2));
+		}
+		
+	}
+	$(document).ready(function () {
+		htmlbodyHeightUpdate()
+		$( window ).resize(function() {
+			htmlbodyHeightUpdate()
+		});
+		$( window ).scroll(function() {
+			height2 = $('.main').height()
+  			htmlbodyHeightUpdate()
+		});
+	});
